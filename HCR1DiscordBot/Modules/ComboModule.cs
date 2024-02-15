ebd0d86bf49066e8ae595ee5878f76f7079fc46b
Roman Salo <saloroman23@gmail.com>
2024-02-15 16:24:02 +0200

    Add *combo command module

---
+++ HCR1DiscordBot/Modules/ComboModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+using HCR1DiscordBot.Services;
+
+namespace HCR1DiscordBot.Modules
+{
+    public class ComboModule : ModuleBase<SocketCommandContext>
+    {
+        private Random _rand = new Random();
+        private JsonReaderService _jsonReader = new JsonReaderService();
+
+        [Command("combo")]
+        public async Task GenerateComboAsync()
+        {
+            var vehicles = _jsonReader.ReadAllVehicles();
+            var stages = _jsonReader.ReadAllStages();
+            await Context.Channel.SendMessageAsync(
+                $"Try to drive with {vehicles[_rand.Next(vehicles.Length)].Name} on {stages[_rand.Next(stages.Length)].Name}!"
+            );
+        }
+    }
+}
