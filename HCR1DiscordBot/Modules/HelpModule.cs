8b1269d5080be8a3863a148c8b6e763743d087d6
Roman Salo <saloroman23@gmail.com>
2024-02-15 16:19:32 +0200

    Add *help command module

---
+++ HCR1DiscordBot/Modules/HelpModule.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace HCR1DiscordBot.Modules
+{
+    public class HelpModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("help")]
+        public async Task HelpAsync()
+        {
+            string text = File.ReadAllText("_help.txt");
+            await Context.Channel.SendMessageAsync(text);
+        }
+    }
+}
