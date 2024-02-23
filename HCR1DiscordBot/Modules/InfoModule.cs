c467b245306c122b8dd4f9c738dc636d79b6d04c
Roman Salo <saloroman23@gmail.com>
2024-02-23 23:46:08 +0200

    Extract *hcrinfo module logic into service

--- HCR1DiscordBot/Modules/InfoModule.cs
+++ HCR1DiscordBot/Modules/InfoModule.cs
@@ -5,85 +5,36 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Globalization;
 using Discord.Commands;
-using HCR1DiscordBot.Services;
 using Discord;
+using HCR1DiscordBot.Services.ModuleServices;
 
 namespace HCR1DiscordBot.Modules
 {
     public class InfoModule : ModuleBase<SocketCommandContext>
     {
-        private Random _rand = new Random();
-        private JsonReaderService _jsonReader = new JsonReaderService(
+        private InfoModuleService _service = new InfoModuleService(
             vehiclesFilePath: "_vehicles.json",
             stagesFilePath: "_stages.json"
         );
-        private InfoSearchService _searchService = new InfoSearchService();
 
         [Command("hcrinfo")]
         public async Task InfoHelpAsync()
         {
-            dynamic item;
-            if (_rand.Next(2) == 0)
-            {
-                var vehicles = _jsonReader.ReadAllVehicles();
-                item = vehicles[_rand.Next(vehicles.Length)];
-            }
-            else
-            {
-                var stages = _jsonReader.ReadAllStages();
-                item = stages[_rand.Next(stages.Length)];
-            }
-
             await Context.Channel.SendMessageAsync(
                 "Type the name of the vehicle or stage you want to check\n" +
-                $"For example: *hcrinfo {item.Name.ToLower()}"
+                $"For example: *hcrinfo {_service.GetRandomVehicleOrStage().Name.ToLower()}"
             );
         }
 
         [Command("hcrinfo")]
         public async Task InfoAsync([Remainder] string parameter)
         {
+            // For formatting numbers with commas, like 1,000,000
             CultureInfo.CurrentCulture = new CultureInfo("en-US");
 
-            StringBuilder sb = new StringBuilder();
-
-            var vehicles = _jsonReader.ReadAllVehicles();
-            dynamic item = _searchService.FindIn(vehicles, parameter);
-            if (item != null)
-            {
-                sb.AppendLine($"{item.Emoji} **{item.Name}**");
-                if (item.Name.ToLower() == "garage")
-                {
-                    sb.AppendLine($"Purchase cost: 300 gems");
-                    sb.AppendLine($"Upgrade cost: Random");
-                    sb.AppendLine($"Total cost (Purchase + Upgrade): Random");
-                }
-                else
-                {
-                    sb.AppendLine($"Purchase cost: {item.PurchaseCost:n0}");
-                    sb.AppendLine($"Upgrade cost: {item.UpgradeCost:n0}");
-                    sb.AppendLine($"Total cost (Purchase + Upgrade): {(item.PurchaseCost + item.UpgradeCost):n0}");
-                }
-                sb.AppendLine($"Fuel Duration: {item.FuelDuration}");
-            }
-            else
-            {
-                var stages = _jsonReader.ReadAllStages();
-                item = _searchService.FindIn(stages, parameter);
-                if (item != null)
-                {
-                    sb.AppendLine($"**{item.Name}**");
-                    sb.AppendLine($"Purchase cost: {item.PurchaseCost:n0}");
-                    sb.AppendLine($"Fuel locations: coming soon...");
-                }
-                else
-                {
-                    sb.AppendLine("Couldn't find such vehicle or stage");
-                }
-            }
-
             var embed = new EmbedBuilder();
-            embed.Description = sb.ToString();
+            embed.Description = _service.GetVehicleOrStageInfo(parameter);
+
             await Context.Channel.SendMessageAsync("", false, embed.Build());
         }
     }
---
+++ HCR1DiscordBot/Services/ModuleServices/InfoModuleService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCR1DiscordBot.Services.ModuleServices
+{
+    public class InfoModuleService
+    {
+        private Random _rand;
+        private JsonReaderService _jsonReaderService;
+        private InfoSearchService _infoSearchService;
+
+        public InfoModuleService(string vehiclesFilePath, string stagesFilePath)
+        {
+            _rand = new Random();
+            _jsonReaderService = new JsonReaderService(vehiclesFilePath, stagesFilePath);
+            _infoSearchService = new InfoSearchService();
+        }
+
+        public dynamic GetRandomVehicleOrStage()
+        {
+            if (_rand.Next(2) == 0)
+            {
+                var vehicles = _jsonReaderService.ReadAllVehicles();
+                return vehicles[_rand.Next(vehicles.Length)];
+            }
+            else
+            {
+                var stages = _jsonReaderService.ReadAllStages();
+                return stages[_rand.Next(stages.Length)];
+            }
+        }
+
+        public string GetVehicleOrStageInfo(string parameter)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            var vehicles = _jsonReaderService.ReadAllVehicles();
+            dynamic item = _infoSearchService.FindIn(vehicles, parameter);
+            if (item != null)
+            {
+                sb.AppendLine($"{item.Emoji} **{item.Name}**");
+                if (item.Name.ToLower() == "garage")
+                {
+                    sb.AppendLine($"Purchase cost: 300 gems");
+                    sb.AppendLine($"Upgrade cost: Random");
+                    sb.AppendLine($"Total cost (Purchase + Upgrade): Random");
+                }
+                else
+                {
+                    sb.AppendLine($"Purchase cost: {item.PurchaseCost:n0}");
+                    sb.AppendLine($"Upgrade cost: {item.UpgradeCost:n0}");
+                    sb.AppendLine($"Total cost (Purchase + Upgrade): {(item.PurchaseCost + item.UpgradeCost):n0}");
+                }
+                sb.AppendLine($"Fuel Duration: {item.FuelDuration}");
+            }
+            else
+            {
+                var stages = _jsonReaderService.ReadAllStages();
+                item = _infoSearchService.FindIn(stages, parameter);
+                if (item != null)
+                {
+                    sb.AppendLine($"**{item.Name}**");
+                    sb.AppendLine($"Purchase cost: {item.PurchaseCost:n0}");
+                    sb.AppendLine($"Fuel locations: coming soon...");
+                }
+                else
+                {
+                    sb.AppendLine("Couldn't find such vehicle or stage");
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
